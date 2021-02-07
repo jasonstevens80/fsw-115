109e2c5fbd4d23403b22fd77a26aa3617a3c4cc4
Jason Stevens <71710082+jasonstevens80@users.noreply.github.com>
2021-02-07 09:06:15 -0500

    Add files via upload

---
+++ week3/app.js
@@ -0,0 +1,23 @@
+console.log('hello world')
+
+const xhr = new XMLHttpRequest()
+
+xhr.open("GET", "https://api.vschool.io/pokemon", true)
+xhr.send()
+
+
+xhr.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+        const JSONdata = xhr.responseText
+        const data = JSON.parse(JSONdata)
+        myData(data.objects[0])
+    }
+}
+
+function myData(arr) {
+    for (let i = 0; i < arr.pokemon.length; i++) {
+        const h1 = document.createElement('h1')
+        h1.textContent = arr.pokemon[i].name
+        document.body.appendChild(h1)
+    }
+}
