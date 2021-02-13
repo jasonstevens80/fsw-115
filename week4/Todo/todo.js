f335cba88f140d54ec308136ec7769dfe7ee4886
Jason Stevens <71710082+jasonstevens80@users.noreply.github.com>
2021-02-13 08:34:44 -0500

    Add files via upload

---
+++ week4/Todo/todo.js
@@ -0,0 +1,20 @@
+axios.get("https://api.vschool.io/jasonstevens/todo")
+    .then(response => {
+        for(let i = 0; i < response.data.length; i++){
+            const h3 = document.createElement('h3')
+
+            if (response.data[i].completed === true){
+                h3.textContent = response.data[i].title
+                h3.style.textDecoration = "line-through"
+                h3.style.textDecorationColor = "red"
+                h3.style.textAlign = "center"
+                h3.style.color="blue"
+            } else {
+                h3.textContent = `${response.data[i].title}`
+                // h3.style.color = "blue"
+                h3.style.textAlign = "center"
+            }
+                document.body.appendChild(h3)
+        }
+    })
+    .catch(error => console.log(error))
